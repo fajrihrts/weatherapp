6034478460b98ede735bbe817a5e50f16a3d1ed3
terongkering <bananaijok@gmail.com>
2022-12-24 23:26:17 +0800

    Deploy To Railway

--- blog/settings.py
+++ blog/settings.py
@@ -109,7 +109,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
